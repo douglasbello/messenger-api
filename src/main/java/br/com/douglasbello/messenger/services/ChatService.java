ed78281577e8ceb360a0593c817ac94d9543a91d
douglasbello <douglasbelloalv1@outlook.com>
2023-05-25 18:07:08 -0300

    ChatService: checkIfAChatBetweenUsersAlreadyExists()

--- src/main/java/br/com/douglasbello/messenger/services/ChatService.java
+++ src/main/java/br/com/douglasbello/messenger/services/ChatService.java
@@ -3,28 +3,36 @@ package br.com.douglasbello.messenger.services;
 import br.com.douglasbello.messenger.dto.ChatDTO;
 import br.com.douglasbello.messenger.entities.Chat;
 import br.com.douglasbello.messenger.entities.Message;
+import br.com.douglasbello.messenger.entities.User;
 import br.com.douglasbello.messenger.repositories.ChatRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ChatService {
     private final ChatRepository chatRepository;
 
     private final MessageService messageService;
+    
+    private final UserService userService;
 
-    public ChatService(ChatRepository chatRepository, MessageService messageService) {
+    public ChatService(ChatRepository chatRepository, MessageService messageService, UserService userService) {
         this.chatRepository = chatRepository;
         this.messageService = messageService;
+        this.userService = userService;
     }
 
+    @Transactional(readOnly = true)
     public List<ChatDTO> findAll() {
         List<Chat> result = chatRepository.findAll();
         return result.stream().map(ChatDTO::new).toList();
     }
 
+    @Transactional
     public ChatDTO insert(Chat obj) {
         ChatDTO chatDTO = new ChatDTO(chatRepository.save(obj));
         return chatDTO;
@@ -36,6 +44,7 @@ public class ChatService {
         entity.getMessages().addAll(obj.getMessages());
     }
 
+    @Transactional
     public ChatDTO update(Integer id, Chat obj) {
         try {
             Chat entity = chatRepository.getReferenceById(id);
@@ -51,5 +60,21 @@ public class ChatService {
         Chat chat = message.getChat();
         chat.getMessages().add(message);
         insert(chat);
+    }
+    
+    @Transactional
+    public boolean checkIfAChatBetweenUsersAlreadyExists(Integer firstUserId, Integer secondUserId) {
+    	List<Chat> chats = chatRepository.findAll();
+    	
+    	for (Chat chat : chats) {
+    		User firstUser = userService.findById(firstUserId);
+    		User secondUser = userService.findById(secondUserId);
+    		
+    		if (chat.getParticipants().contains(firstUser) && chat.getParticipants().contains(secondUser)) {
+    			return true;
+    		}
+    	}
+    	
+    	return false;
     }
 }
